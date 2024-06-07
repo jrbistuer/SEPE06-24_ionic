ef49a0f71295323f2e93f652fa1c3e3fc49284a7
jrbistuer <jrbistuer@gmail.com>
2024-06-07 17:09:01 +0200

    logout

--- src/app/pages/private/tab1/tab1.page.ts
+++ src/app/pages/private/tab1/tab1.page.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonButton, IonIcon } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { logOutOutline } from 'ionicons/icons';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonIcon, IonButton, IonButtons, IonHeader, IonToolbar, IonTitle, IonContent],
 })
 export class Tab1Page {
-  constructor() {}
+
+  constructor(private authService: AuthService,
+    private router: Router
+  ) {
+    addIcons({
+      logOutOutline
+    });
+  }
+
+  logOut() {
+    this.authService.logout().then(() => {
+      this.router.navigateByUrl('/login', { replaceUrl: true });
+    })
+  }
 }
